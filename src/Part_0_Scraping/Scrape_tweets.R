7e2d6e8071c446d3c610689c007ed54d2b342f1b
klazarov <Konstantin.Lazarov@UGent.be>
2022-03-13 22:45:43 +0100

    Using new scraper

--- src/Part_0_Scraping/Scrape_tweets.R
+++ src/Part_0_Scraping/Scrape_tweets.R
@@ -6,6 +6,9 @@ setwd("/Users/konstantinlazarov/Desktop/SMWA/Group_Assignement")
 if (!require("pacman")) install.packages("pacman") ; require("pacman")
 p_load(tidyverse, rtweet, httpuv, stringr, qdap, httr, wordcloud2, tm, tidytext, wordcloud)
 
+#install.packages("twitteR") 
+library(twitteR)
+
 
 source('tokensandkeys.R')
 
@@ -13,12 +16,23 @@ token <- get_token()
 
 search.string <- "#Starbucks"
 
-tweets <- search_tweets(search.string, n = 18000, 
+tweets <- search_tweets(search.string, n = 5000, 
                         include_rts = FALSE,
                         retryonratelimit = FALSE,
                         lang = 'en',
                         token = get_token())
 
+
+#https://developer.twitter.com/en/portal/projects/1495455141645926414/apps
+tweets2 <- searchTwitter(search.string, n = 18000, 
+                        since = "2022-03-01") 
+list_starbucks = twListToDF(tweets2)
+plot(list_starbucks$created)
+min(list_starbucks$created)
+max(list_starbucks$created)
+
+setwd("/Users/konstantinlazarov/Documents/GitHub/SMWA_Performance/data")
+save(tweets2, file = "Scrape5_03Till13_03.RData")
 
 
 #https://www.rdocumentation.org/packages/rtweet/versions/0.7.0/topics/search_tweets 
