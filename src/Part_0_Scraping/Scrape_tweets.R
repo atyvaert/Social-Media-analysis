266cd6cc1f58c5d598e02b7e4dcad863aaafcf19
wodwitte <wodwitte.Dewitte@UGent.be>
2022-03-22 11:11:28 +0100

    hey

--- src/Part_0_Scraping/Scrape_tweets.R
+++ src/Part_0_Scraping/Scrape_tweets.R
@@ -49,36 +49,6 @@ tweets$retweet_location
 plot(tweets_table$created_at)
 
 
-text <- tweets_data(tweets) %>% pull(text)
-myCorpus <- Corpus(VectorSource(text))
-
-myCorpus <- Corpus(VectorSource(text)) %>%
-  tm_map(content_transformer(str_to_lower)) %>%
-  tm_map(removePunctuation) %>%
-  tm_map(removeNumbers) %>%
-  tm_map(stripWhitespace)
-myStopwords <- c(stopwords('english'))
-myCorpus <- tm_map(myCorpus, removeWords, myStopwords)
-
-
-tdm <- TermDocumentMatrix(myCorpus)
-m <- as.matrix(tdm)
-v <- sort(rowSums(m),decreasing=TRUE)
-d <- tibble(word = names(v),freq=v)
-
-d <- d %>% filter(word != tolower(search.string)) %>% arrange(desc(freq))
-#d <- d %>% filter(word != "tesla") %>% arrange(desc(freq))
-#d <- d %>% filter(word != "elonmusk") %>% arrange(desc(freq))
-#d <- d %>% filter(word != "musk") %>% arrange(desc(freq))
-#d <- d %>% filter(word != "elon") %>% arrange(desc(freq))
-#d <- d %>% filter(word != "you") %>% arrange(desc(freq))
-
-options(warn=-1) #turn warnings off
-wordcloud(d$word,d$freq)
-options(warn=0) #turn warnings back on
-
-wordcloud_starbucks <- wordcloud2(d)
-
 
 
 
