4aa691f7ed96e3f95e8085c8a4af97a0002b5a52
BeJonck <bert.jonckheere99@gmail.com>
2022-03-25 09:35:00 +0100

    adjusting time

--- src/Part_0_Scraping/Merging.R
+++ src/Part_0_Scraping/Merging.R
@@ -49,18 +49,42 @@ load("Scrape_18_03.Rdata")
 tweets23 <- as.data.frame(tweets[, c("user_id", "created_at", "text")])
 load("Scrape2_22_03.Rdata")
 tweets24 <- as.data.frame(tweets[, c("user_id", "created_at", "text")])
-
+load("Scrape_24_03.Rdata")
+tweets25 <- as.data.frame(tweets[, c("user_id", "created_at", "text")])
 
 
 all_tweets <- rbind(tweets1, tweets3, tweets4, tweets5, tweets6, tweets7, tweets8, tweets9, tweets10, tweets11, tweets12, tweets13, tweets14,
-                    tweets15, tweets16, tweets17, tweets18, tweets19, tweets20, tweets21, tweets22, tweets23, tweets24)
+                    tweets15, tweets16, tweets17, tweets18, tweets19, tweets20, tweets21, tweets22, tweets23, tweets24, tweets25)
 colnames(all_tweets) <- c("user_id", "date", "text")
-
-all_tweets$date <- format(as.POSIXct(all_tweets$date,format='%m/%d/%Y %H:%M:%S'),format='%m/%d/%Y')
-
-class(all_tweets)
 
 sum(duplicated(all_tweets))
 all_tweets <- all_tweets[!duplicated(all_tweets), ]
+
+
+all_tweets$date <- ifelse(as.integer(format(all_tweets$date, format = "%H%M")) <= 1330,
+                          ifelse(as.integer(format(all_tweets$date, format = "%M")) < 30,
+                                 all_tweets$date + 60 - as.integer(format(all_tweets$date, format = "%S")) + (30 - (as.integer(format(all_tweets$date, format = "%M")) + 1)) * 60 + (14 - as.integer(format(all_tweets$date, format = "%H"))) * 60 * 60,
+                                 all_tweets$date + 60 - as.integer(format(all_tweets$date, format = "%S")) + (90 - (as.integer(format(all_tweets$date, format = "%M")) + 1)) * 60 + (14 - (as.integer(format(all_tweets$date, format = "%H")) + 1)) * 60 * 60),
+                          ifelse(as.integer(format(all_tweets$date, format = "%H%M")) <= 1930,
+                                 ifelse(as.integer(format(all_tweets$date, format = "%M")) < 30,
+                                        all_tweets$date + 60 - as.integer(format(all_tweets$date, format = "%S")) + (30 - (as.integer(format(all_tweets$date, format = "%M")) + 1)) * 60 + 60 * 60,
+                                        all_tweets$date + 60 - as.integer(format(all_tweets$date, format = "%S")) + (90 - (as.integer(format(all_tweets$date, format = "%M")) + 1)) * 60 + 60 * 60),
+                                 ifelse(as.integer(format(all_tweets$date, format = "%M")) < 30,
+                                        all_tweets$date + 60 - as.integer(format(all_tweets$date, format = "%S")) + (30 - (as.integer(format(all_tweets$date, format = "%M")) + 1)) * 60 + (38 - as.integer(format(all_tweets$date, format = "%H"))) * 60 * 60,
+                                        all_tweets$date + 60 - as.integer(format(all_tweets$date, format = "%S")) + (90 - (as.integer(format(all_tweets$date, format = "%M")) + 1)) * 60 + (38 - (as.integer(format(all_tweets$date, format = "%H")) + 1)) * 60 * 60)))
+
+all_tweets$date <- all_tweets$date - 60 * 60
+
+all_tweets$date <- as.POSIXct(all_tweets$date, format = "%Y-%m-%d %H:%M:%S", origin = "1970-01-01 00:00:00")
+
+all_tweets$weekday <- weekdays(all_tweets$date)
+
+all_tweets$date <- ifelse(all_tweets$weekday == "zaterdag", all_tweets$date + 48 * 60 *60,
+                          ifelse(all_tweets$weekday == "zondag", all_tweets$date + 24 * 60 * 60, all_tweets$date))
+
+all_tweets$date <- as.POSIXct(all_tweets$date, format = "%Y-%m-%d %H:%M:%S", origin = "1970-01-01 00:00:00")
+
+all_tweets <- subset(all_tweets, select = -c(weekday))
+
 
 save(all_tweets, file = "all_tweets.Rdata")
